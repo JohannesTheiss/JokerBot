76eacdba19726ff92f7b828b3f75a3c2ca296479
Johannes <johannestheiss00@gmail.com>
2021-05-05 20:23:31 +0200

    update logger

--- log/logger.py
+++ log/logger.py
@@ -1,26 +1,33 @@
 import os
 import logging
 
+# CONFIG
+LOG_FILE   = "jokerbot"
+LOG_DIR    = "logs"
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+def createDir(name):
+    path = os.path.dirname(os.path.realpath(__file__)) + '/' + name
+    try:
+        if not os.path.isdir(path):
+            os.mkdir(path)
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    return path
+
+
 # Create a custom logger
-logger = logging.getLogger("jokerbot")
+logger = logging.getLogger(LOG_FILE)
 
 # Create handlers
 c_handler = logging.StreamHandler()
-
-path = os.path.dirname(os.path.realpath(__file__)) + '/logs'
-try:
-    os.mkdir(path)
-except OSError:
-    print ("Creation of the directory %s failed" % path)
-
-f_handler = logging.FileHandler(path + '/jokerbot.log')
-
+f_handler = logging.FileHandler(createDir(LOG_DIR) + '/{}.log'.format(LOG_FILE))
 c_handler.setLevel(logging.DEBUG)
 f_handler.setLevel(logging.ERROR)
 
 # Create formatters and add it to handlers
-c_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+c_format = logging.Formatter(LOG_FORMAT)
+f_format = logging.Formatter(LOG_FORMAT)
 c_handler.setFormatter(c_format)
 f_handler.setFormatter(f_format)
 
