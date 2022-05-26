7886fd6bc515903affaa7534353db03e6b0515ed
JohannesTheiss <johannestheiss00@gmail.com>
2022-05-26 14:30:24 +0200

    remove comments

--- src/joker_bot.py
+++ src/joker_bot.py
@@ -1,30 +1,5 @@
-import sys
-
-from pathlib import Path
-
-# discord.py imports
 import discord
 from discord.ext import commands
-
-
-# append the src dir to the PATH env. var.
-#file_path = str(Path(__file__).parent)
-#sys.path.append(file_path)
-
-# import local files
-#from log import *
-#from roles.roles import Roles
-#from util.util import Util
-#from util.help import HelpCog
-
-
-
-# setting up the Bot
-#intents = discord.Intents.all()
-#PREFIX = '!'
-# bot = commands.Bot(command_prefix=PREFIX,
-                   # intents=intents,
-                   # help_command=None)
 
 class JokerBot(commands.Bot):
 
@@ -57,12 +32,4 @@ class JokerBot(commands.Bot):
                                         name="187",
                                         url="https://www.youtube.com/watch?v=CzlOERhLEFw",
                                         details="lol ok"))
-
-        #user = await client.fetch_user("Dan!el#7786")
-        #user = await client.fetch_user("7950")
-        #await user.send("Hello there!")
-
-
-
-#bot.run(TOKEN)
 
--- src/util/help.py
+++ src/util/help.py
@@ -1,18 +1,17 @@
 import discord
 from discord.ext import commands
 
-PREFIX = '!'
-
 class HelpCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.prefix = bot.command_prefix
 
     @commands.command()
     async def help(self, ctx):
         embed = discord.Embed(title="Help commands", description="Shows various help commands")
-        embed.add_field(name="Show this help", value = f"`{PREFIX}help`", inline = False)
+        embed.add_field(name="Show this help", value = f"`{self.prefix}help`", inline = False)
         embed.add_field(name="Help with a Plugin",
-                        value=f"`{PREFIX}help <plugins>`\ne.g. `{PREFIX}help roles`",
+                        value=f"`{self.prefix}help <plugins>`\ne.g. `{self.prefix}help roles`",
                         inline=False)
         embed.add_field(name="Installed Plugins",
                         value=f":point_right: util\n:point_right: roles",
